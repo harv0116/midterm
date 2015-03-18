5b263cba3a01b057a98b38681f8522f464dd70b8
Paul Harvey <harv0116@algonquinlive.com>
2015-03-18 10:31:56 -0400

    update

--- www/js/index.js
+++ www/js/index.js
@@ -1,51 +1,73 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
+
 var app = {
-    // Application Constructor
+  	name: "Midterm App",
+  	version: "1.2.3",
+	links: [],
+	numPages: 0,
+	pageTime: 500,
+	
     initialize: function() {
         this.bindEvents();
     },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
+
     bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
+		document.addEventListener('deviceready', this.onDeviceReady, false);
     },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
+
     onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
+		
+		var options = new ContactFindOptions();
+		options.filter = "";
+		options.multiple = true;
+		var fields = ["displayName"]; 
+		navigator.contacts.find(fields, app.successFunc, app.errFunc, options);	
+		document.getElementById("btnCancel").addEventListener("click", app.cancel);
+    	document.getElementById("btnSave").addEventListener("click", app.save);
+	 
+  	},
+	successFunc: function( matches ){
+		var div = document.getElementById("displaycontact");
+		var nameul = document.createElement("ul");
+		nameul.setAttribute("data-role","listview");
+		
+		for (i=0; i<matches.length; i++)
+		{	
+			console.log("Printing Name");	
+			nameul.innerHTML += '<li data-ref="' + i + '">' + matches[i].displayName + '</li>';
+		}
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
+		div.appendChild(nameul);
+		console.log("Appended");
+		document.querySelector("[data-role=listview]").addEventListener("click", app.edit);
+   	 	
+		 
+	},
+	errFunc: function ( ) {
+		alert("The contact could not be found");
+	},
+	cancel: function(ev){
+		document.querySelector("[data-role=modal]").style.display="none";
+		document.querySelector("[data-role=overlay]").style.display="none";
+	},
+	save: function(ev){
+		document.querySelector("[data-role=modal]").style.display="none";
+		document.querySelector("[data-role=overlay]").style.display="none";
+	},
+	edit: function(ev){
+		ev.stopPropagation();
+		document.querySelector("[data-role=modal]").style.display="block";
+		document.querySelector("[data-role=overlay]").style.display="block";
+	
+		var item = ev.target.getAttribute("data-ref");
+		var itemVal = ev.target.innerHTML;
+		document.getElementById("list").value = item;
+		document.querySelector("[data-role=modal] h3").innerHTML = "Editing " + itemVal;
+		
+		// display all of contact information here.  Name & 2 Phone Numbers.
+	}
+	
 
-        console.log('Received Event: ' + id);
-    }
 };
-
 app.initialize();
+
+
